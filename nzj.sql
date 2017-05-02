d73386a1d76fc0639598bf8bf223e183d8081c74
sudozgj <13714515160@163.com>
2017-05-02 19:00:16 +0800

    数据库脚本

--- nzj.sql
+++ nzj.sql
@@ -10,10 +10,274 @@ Target Server Type    : MYSQL
 Target Server Version : 50535
 File Encoding         : 65001
 
-Date: 2017-04-29 18:38:21
+Date: 2017-05-02 18:58:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `appliance`
+-- ----------------------------
+DROP TABLE IF EXISTS `appliance`;
+CREATE TABLE `appliance` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of appliance
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt`;
+CREATE TABLE `aunt` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `sign` varchar(255) NOT NULL,
+  `native` varchar(255) NOT NULL,
+  `sex` int(11) NOT NULL,
+  `education` varchar(255) NOT NULL,
+  `marriage` int(11) NOT NULL,
+  `nation` varchar(255) NOT NULL,
+  `height` double NOT NULL,
+  `weight` double NOT NULL,
+  `sigh` double NOT NULL,
+  `idnumber` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_appliance`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_appliance`;
+CREATE TABLE `aunt_appliance` (
+  `id` bigint(20) NOT NULL,
+  `aunt_id` bigint(20) NOT NULL,
+  `appliance_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_appliance
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_certificate`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_certificate`;
+CREATE TABLE `aunt_certificate` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `certificate_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_certificate
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_contact`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_contact`;
+CREATE TABLE `aunt_contact` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `contact_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_contact
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_cooking`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_cooking`;
+CREATE TABLE `aunt_cooking` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `cooking_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_cooking
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_job`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_job`;
+CREATE TABLE `aunt_job` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `job_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_job
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_language`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_language`;
+CREATE TABLE `aunt_language` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `language_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_language
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_photo`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_photo`;
+CREATE TABLE `aunt_photo` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `aunt_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_photo
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_skill`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_skill`;
+CREATE TABLE `aunt_skill` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `skill_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_skill
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `aunt_work`
+-- ----------------------------
+DROP TABLE IF EXISTS `aunt_work`;
+CREATE TABLE `aunt_work` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `aunt_id` bigint(20) NOT NULL,
+  `work_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of aunt_work
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `certificate`
+-- ----------------------------
+DROP TABLE IF EXISTS `certificate`;
+CREATE TABLE `certificate` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of certificate
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `contact`
+-- ----------------------------
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `relation` varchar(255) NOT NULL,
+  `workstatus` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of contact
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `cooking`
+-- ----------------------------
+DROP TABLE IF EXISTS `cooking`;
+CREATE TABLE `cooking` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cooking
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `job`
+-- ----------------------------
+DROP TABLE IF EXISTS `job`;
+CREATE TABLE `job` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of job
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `language`
+-- ----------------------------
+DROP TABLE IF EXISTS `language`;
+CREATE TABLE `language` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of language
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `skill`
+-- ----------------------------
+DROP TABLE IF EXISTS `skill`;
+CREATE TABLE `skill` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of skill
+-- ----------------------------
+
 -- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
@@ -64,3 +328,18 @@ CREATE TABLE `user_detail` (
 INSERT INTO `user_detail` VALUES ('1', '4', 'zgj', 'sy', '小明', '345345435', '123', 'sa', 'http://fsdfa.jpg', '');
 INSERT INTO `user_detail` VALUES ('2', '8', 'r', 'ew', '23', '1239', '7603', 'g3', '', 'http23');
 INSERT INTO `user_detail` VALUES ('3', '7', 'gggggg23g', 'ew', '23', '1239', '7603', 'g3', null, 'http23');
+
+-- ----------------------------
+-- Table structure for `work`
+-- ----------------------------
+DROP TABLE IF EXISTS `work`;
+CREATE TABLE `work` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `time` varchar(255) NOT NULL,
+  `work` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of work
+-- ----------------------------
